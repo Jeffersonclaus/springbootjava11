358739956a704020a9cbb7d0928090d0b43bb7f9
Jefferson Claus <jeffersonclaus@gmail.com>
2021-09-15 13:48:02 -0300

    Inicio projeto

---
+++ src/test/java/com/clausweb/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clausweb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
