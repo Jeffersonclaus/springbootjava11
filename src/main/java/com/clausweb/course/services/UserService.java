942400dc6a9e9cf609128573cf35631164a4f752
Jefferson Claus <jeffersonclaus@gmail.com>
2021-09-16 14:45:48 -0300

    Camada de serviço e component registration

---
+++ src/main/java/com/clausweb/course/services/UserService.java
@@ -0,0 +1,34 @@
+package com.clausweb.course.services;
+import java.util.List;
+import java.util.Optional;
+
+import com.clausweb.course.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.clausweb.course.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired 
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		
+	return repository.findAll();	
+		
+	
+	}
+	
+	public User FindById(Long id) {
+		Optional <User> obj = repository.findById(id);
+		return obj.get(); //retorna get dentro do obj <USER>
+		
+	}
+	
+	
+
+}
