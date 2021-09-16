b83167d5c6ea19ccfd6a501e680c24d66bd6562b
Jefferson Claus <jeffersonclaus@gmail.com>
2021-09-16 16:25:16 -0300

    CLASS Order, Instant ISO 8601

---
+++ src/main/java/com/clausweb/course/services/OrderService.java
@@ -0,0 +1,35 @@
+package com.clausweb.course.services;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.clausweb.course.entities.Order;
+import com.clausweb.course.entities.User;
+import com.clausweb.course.repositories.OrderRepository;
+import com.clausweb.course.repositories.UserRepository;
+
+
+@Service
+public class OrderService {
+	
+	@Autowired 
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		
+	return repository.findAll();	
+		
+	
+	}
+	
+	public Order FindById(Long id) {
+		Optional <Order> obj = repository.findById(id);
+		return obj.get(); //retorna get dentro do obj <USER>
+		
+	}
+	
+	
+
+}
